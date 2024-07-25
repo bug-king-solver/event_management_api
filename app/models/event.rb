e3d1dc2e980840293d629da593d03ecfe67a79ec
Daniel <jannytechservice00@outlook.com>
2024-07-25 03:50:38 +0300

    feat: add request validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
     has_many :participants
+    
     validates :title, :capacity, presence: true
+    validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,6 @@
 class Participant < ApplicationRecord
   belongs_to :event
+  
   validates :name, :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
