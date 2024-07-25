4e83780b75c1d9bf23fc50d90da94cfaaad3f0d0
Daniel <jannytechservice00@outlook.com>
2024-07-25 03:10:20 +0300

    feat: setup associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    has_many :participants
+    validates :title, :capacity, presence: true
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,4 @@
 class Participant < ApplicationRecord
   belongs_to :event
+  validates :name, :email, presence: true
 end
